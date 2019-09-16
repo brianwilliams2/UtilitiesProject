6b14fc727649809b0708102bab4a133c87148099
brianwilliams2 <46699166+brianwilliams2@users.noreply.github.com>
2019-09-15 19:16:26 -0600

    adding utilities project

---
+++ Utilities/Assets/TextCode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextCode : MonoBehaviour
+{
+    //Properties are private by default
+    public InputField inputText;
+    public Text outputText;
+    public Button textButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       outputText.text = " ";
+    }
+
+    public void ProcessText()
+    {        
+        outputText.text = Utilities.ProcessText(inputText.text);
+    }
+    
+}
